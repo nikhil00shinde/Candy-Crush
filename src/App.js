17076eba2534f75ccaa5630a3186885c1c3a0b68
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-10-22 03:31:37 +0530

    random color array

---
+++ src/App.js
@@ -0,0 +1,16 @@
+const width = 8;
+const candyColors = ["blue", "green", "orange", "purple", "red", "yellow"];
+
+const App = () => {
+	const createBoard = () => {
+		const randomColorArrangement = [];
+		for (let i = 0; i < width * width; i++) {
+			const randomColor =
+				candyColors[Math.floor(Math.random() * candyColors.length)];
+			randomColorArrangement.push(randomColor);
+		}
+	};
+	return <div></div>;
+};
+
+export default App;
