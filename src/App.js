18987c65635bb6c66ccf86d0f662983f130ae7b2
Nikhil Shinde <nikhilshinde00@outlook.com>
2022-02-04 18:59:03 +0530

    checking row and move into square below

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ const App = () => {
 			}
 		}
 	};
-	
+
 	const checkForColumnOfThree = () => {
 		for (let i = 0; i < 47; i++) {
 			const columnOfThree = [i, i + width, i + width * 2];
@@ -33,6 +33,65 @@ const App = () => {
 				)
 			) {
 				columnOfThree.forEach((square) => (currColorArrangement[square] = ""));
+			}
+		}
+	};
+
+	const checkForRowOfThree = () => {
+		for (let i = 0; i < 64; i++) {
+			const rowOfThree = [i, i + 1, i + 2];
+			const decidedColor = currColorArrangement[i];
+
+			const notValid = [
+				6, 7, 14, 15, 22, 23, 30, 31, 38, 39, 46, 47, 54, 55, 62, 63,
+			];
+
+			if (notValid.includes(i)) continue;
+			if (
+				rowOfThree.every(
+					(square) => currColorArrangement[square] === decidedColor
+				)
+			) {
+				rowOfThree.forEach((square) => (currColorArrangement[square] = ""));
+			}
+		}
+	};
+
+	const checkForRowOfFour = () => {
+		for (let i = 0; i < 64; i++) {
+			const rowOfFour = [i, i + 1, i + 2, i + 3];
+			const decidedColor = currColorArrangement[i];
+
+			const notValid = [
+				5, 6, 7, 13, 14, 15, 21, 22, 23, 29, 30, 31, 37, 38, 39, 45, 46, 47, 53,
+				54, 55, 61, 62, 63,
+			];
+
+			if (notValid.includes(i)) continue;
+			if (
+				rowOfFour.every(
+					(square) => currColorArrangement[square] === decidedColor
+				)
+			) {
+				rowOfFour.forEach((square) => (currColorArrangement[square] = ""));
+			}
+		}
+	};
+
+	const moveIntoSquareBelow = () => {
+		for (let i = 0; i < 64 - width; i++) {
+			const firstRow = [0, 1, 2, 3, 4, 5, 6, 7];
+
+			const isFirstRow = firstRow.includes(i);
+
+			if (isFirstRow && currColorArrangement[i] === "") {
+				let randomNumber = Math.floor(Math.random() * candyColors.length);
+				currColorArrangement[i] = candyColors[randomNumber];
+			}
+
+			if (currColorArrangement[i + width] === "") {
+				currColorArrangement[i + width] = currColorArrangement[i];
+				currColorArrangement[i] = "";
 			}
 		}
 	};
@@ -53,19 +112,19 @@ const App = () => {
 	}, []);
 
 	useEffect(() => {
-		console.log(1);
 		const timer = setInterval(() => {
-			console.log(2);
 			checkForColumnOfFour();
 			checkForColumnOfThree();
+			checkForRowOfThree();
+			checkForRowOfFour();
+			moveIntoSquareBelow();
 			setCurrentColorArrangement([...currColorArrangement]);
 		}, 1000);
 
 		return () => {
-			console.log(3);
 			clearInterval(timer);
 		};
-	}, [checkForColumnOfFour, checkForColumnOfThree, currColorArrangement]);
+	}, [currColorArrangement]);
 
 	return (
 		<div className="app">
